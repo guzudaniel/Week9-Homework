87383f3e66307d5b6ffaf1fc80fc330973346866
Daniel Guzu <daniel.guzu@emanuel.ro>
2021-05-20 08:13:16 +0300

    first commit

---
+++ Data/Week9_TemaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Week9_Tema.Models;
+
+namespace Week9_Tema.Data
+{
+    public class Week9_TemaContext : DbContext
+    {
+        public Week9_TemaContext (DbContextOptions<Week9_TemaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Week9_Tema.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Week9_Tema.Models
+{
+    public class Movie
+    {
+
+        public Guid ID { get; set; }
+        public string title { get; set; }
+        public int year { get; set; }
+        public string type { get; set; }
+    }
+}
